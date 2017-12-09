d5142d12c3dfd7c8e04585475489b021503172c2
Trevor Nelson <tsnelson86@gmail.com>
2017-12-09 13:57:24 -0700

    basic structure

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const mongoose = require('mongoose');
+
+const Post = require('./models/Post');
+
+const PORT = 8080;
+const app = express();
+
+app.use(bodyParser,json());
+
+app.get('/api', (req, res) => res.send('Hello World!'));
+
+mongoose.connect('mongodb://localhost/forum');
+
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}`));
