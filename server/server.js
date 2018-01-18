0f22910fda77d76f1947cd9314fdd63ccdeb5919
Brian Russell <brianrobertrussell@gmail.com>
2018-01-17 20:05:30 -0700

    heroku config

--- server/server.js
+++ server/server.js
@@ -13,7 +13,13 @@ const app = express();
 app.use(bodyParser.json());
 
 mongoose.Promise = Promise;
-mongoose.connect('mongodb://localhost/pillowApp');
+
+
+if(process.env.MONGODB_URI){
+	mongoose.connect(process.env.MONGODB_URI);
+} else {
+	mongoose.connect('mongodb://localhost/pillowApp');
+}
 
 var user = {};
 
