5993f852d1f16563b04dc71e7fd69e814cf28aaa
Brian Russell <brianrobertrussell@gmail.com>
2018-01-17 21:06:59 -0700

    corrected env setting

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ mongoose.Promise = Promise;
 
 
 if(process.env.MONGODB_URI){
-	mongoose.connect(process.env.MONGODB_URIcd .);
+	mongoose.connect(process.env.MONGODB_URI);
 } else {
 	mongoose.connect('mongodb://localhost/pillowApp');
 }
